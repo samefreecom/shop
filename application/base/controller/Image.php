eb5ad9599bf22b67ecd6f009335ab7b471536f07
mengdz <83398609@qq.com>
2023-05-24 21:27:10 +0800

    u

--- application/base/controller/Image.php
+++ application/base/controller/Image.php
@@ -7,11 +7,13 @@
         {
             $image = new \Lib_Image();
             $image->barcode($_GET['no'], sfret('size', '1-40'));
+            sfquit();
         }
         
         public function qrcode()
         {
             $image = new \Lib_Image();
             $image->qrcode($_GET['no'], 'png', null, sfret('size', '3'));
+            sfquit();
         }
     }
